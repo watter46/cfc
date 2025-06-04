ff2875b59764932c7cea0ef18a3cea96cd5cb7c2
watter46 <w.aoki.14@gmail.com>
2025-06-04 10:09:49 +0900

    delete, fix, add: nextjsからReactへ変更 不必要なファイルを削除 Reactの設定ファイルを追加する

---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    // External Publication
+    host: true,
+    // You can change the port for starting up.
+    port: 3000,
+  },
+});
